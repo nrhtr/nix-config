3475651ef55b4b15aaf455284630c1317b7c36be
Jeremy Parker <jparker@squiz.net>
2019-04-06 13:40:24 +1100

    add config

---
+++ conf/configuration.nix
@@ -0,0 +1,62 @@
+{ config, pkgs, ... }: {
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+  ];
+
+  boot.loader.grub.device = "/dev/vda";   # (for BIOS systems only)
+  #boot.loader.systemd-boot.enable = true; # (for UEFI systems only)
+
+  # Note: setting fileSystems is generally not
+  # necessary, since nixos-generate-config figures them out
+  # automatically in hardware-configuration.nix.
+  #fileSystems."/".device = "/dev/disk/by-label/nixos";
+
+  # Enable the OpenSSH server.
+  services.sshd.enable = true;
+
+  services.openssh.ports = [622];
+  security.sudo.wheelNeedsPassword = false;
+  networking.firewall.logRefusedConnections = false;
+  networking.firewall.allowPing = true;
+
+  networking.firewall.allowedTCPPortRanges = [
+    { from = 80;  to = 80;  } # HTTP
+    { from = 443; to = 443; } # HTTPS
+  ];
+
+  services.nginx.enable = true;
+  services.nginx.virtualHosts = {
+    "jenga.xyz" = {
+      forceSSL = true;
+      enableACME = true;
+      root = "/var/www/jenga.xyz";
+    };
+
+    "boycrisis.net" = {
+      forceSSL = true;
+      enableACME = true;
+      root = "/var/www/boycrisis.net";
+    };
+
+    "paulfl.art" = {
+      forceSSL = true;
+      enableACME = true;
+      root = "/var/www/paulfl.art";
+    };
+  };
+
+  environment.systemPackages = with pkgs; [
+      vim
+      git
+      htop
+  ];
+
+  users.users.jenga = {
+    isNormalUser = true;
+    home = "/home/jenga";
+    description = "Jeremy Parker";
+    extraGroups = [ "wheel" "networkmanager" ];
+    openssh.authorizedKeys.keys = [ "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBBroC7fhTdO17jn7U4FE97IFUYE4NfWxFcxax6bwVzsIXBRCQ9mYlNvmYokWTYX+rlSVi1ifpiwaveJHqcZX4hM=" ];
+  };
+}
