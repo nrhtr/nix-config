a14cb7abe8dd12d0de286c6a73b5dc3971994797
Jeremy Parker <jparker@squiz.net>
2019-04-07 09:00:03 +1000

    fix typo + with fixed hook

--- conf/configuration.nix
+++ conf/configuration.nix
@@ -19,7 +19,7 @@
   services.openssh.ports = [622];
   security.sudo.wheelNeedsPassword = false;
   networking.firewall.logRefusedConnections = false;
-  networking.firewall.allowPing = truee;
+  networking.firewall.allowPing = true;
 
   networking.firewall.allowedTCPPortRanges = [
     { from = 80;  to = 80;  } # HTTP
