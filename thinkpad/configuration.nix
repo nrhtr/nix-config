1c248ba2ea3d7a4c51a793fcadafdbd1bdcb3c0f
Jeremy Parker <jeremy@jenga.xyz>
2019-04-10 07:12:40 +1000

    update thinkpad config

--- thinkpad/configuration.nix
+++ thinkpad/configuration.nix
@@ -1,23 +1,36 @@
-{ config, pkgs, ... }: {
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
   imports = [
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
     ./wireguard.nix
   ];
 
-  #boot.loader.grub.device = "/dev/vda";   # (for BIOS systems only)
-  boot.loader.systemd-boot.enable = true; # (for UEFI systems only)
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
 
-  # Note: setting fileSystems is generally not
-  # necessary, since nixos-generate-config figures them out
-  # automatically in hardware-configuration.nix.
-  #fileSystems."/".device = "/dev/disk/by-label/nixos";
+  networking.hostName = "thinkpad"; # Define your hostname.
+  networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Use a swapfile, because we don't want to bother with another LUKS partition
+  swapDevices = [
+    { device = "/swapfile"; size = 10000; }
+  ];
 
   system.autoUpgrade.enable = true;
   system.autoUpgrade.channel = "https://nixos.org/channels/nixos-18.09-small";
 
+  # Set your time zone.
+  time.timeZone = "Australia/Sydney";
+
   # Disable the OpenSSH server.
-  services.sshd.enable = false;
+  services.sshd.enable = true;
   security.sudo.wheelNeedsPassword = false;
   networking.firewall.logRefusedConnections = true;
 
