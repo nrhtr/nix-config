9e78a2d3c39abcdae71a145b08c5603c9f15f6c1
Jeremy Parker <jeremy@jenga.xyz>
2019-07-06 08:45:03 +0000

    minecraft: default port

--- minecraft.jenga.xyz/configuration.nix
+++ minecraft.jenga.xyz/configuration.nix
@@ -40,7 +40,6 @@
       jenga = "de7e40bc-9fa7-486f-9e7e-cbd337e2ef74";
     };
     serverProperties = {
-      server-port = 43000;
       difficulty = 3;
       gamemode = 1;
       max-players = 4;
