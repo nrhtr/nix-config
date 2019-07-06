b1f87eafb7a34cb8a5047d25dce8c1b75f2a6cdd
Jeremy Parker <jeremy@jenga.xyz>
2019-07-06 06:56:14 +0000

    new squiz MBA wg-pubkey

--- nix01.jenga.xyz/wireguard.nix
+++ nix01.jenga.xyz/wireguard.nix
@@ -28,7 +28,7 @@
 
       peers = [
         { # Squiz MBA
-          publicKey = "czbfummmWDNTWVXzDo2uSfp1/4oYnWS4rI7xUXPbCVg=";
+          publicKey = "zCc5xpI0ul94qOjiOoHdn1VHEUBnTCfljdjLNoeoFTs=";
           # List of IPs assigned to this peer within the tunnel subnet. Used to configure routing.
           allowedIPs = [ "10.100.0.2/32" ];
         }
