e9a7ab0ea85b51b97c88bcbec122dd263a59a5de
Jeremy Parker <jeremy@jenga.xyz>
2019-07-06 06:56:01 +0000

    add wireguard containers

--- nix01.jenga.xyz/configuration.nix
+++ nix01.jenga.xyz/configuration.nix
@@ -59,4 +59,70 @@
       root = "/var/www/paulfl.art";
     };
   };
+
+  # Store wg peer information
+  containers.wg-etcd01 = {
+    privateNetwork = true;
+    localAddress = "10.60.0.10";
+    hostAddress  = "10.60.1.10";
+    config = { config, pkgs, ... }:
+    {
+      networking.firewall.allowedTCPPorts = [ 2379 2380 ];
+      services.etcd = {
+        enable = true;
+        name = "wg-etcd01";
+        initialAdvertisePeerUrls = ["http://10.60.0.10:2380"];
+        listenPeerUrls           = ["http://10.60.0.10:2380"];
+        advertiseClientUrls      = ["http://10.60.0.10:2379"];
+        listenClientUrls         = ["http://10.60.0.10:2379"];
+        initialCluster           = ["wg-etcd01=http://10.60.0.10:2380"
+                                    "wg-etcd02=http://10.60.0.20:2380"];
+      };
+      systemd.services.etcd.serviceConfig.Restart = "always";
+      systemd.services.etcd.serviceConfig.TimeoutStartSec = 60;
+      systemd.services.etcd.serviceConfig.TimeoutStopSec = 60;
+    };
+  };
+
+
+  containers.wg-etcd02 = {
+    privateNetwork = true;
+    localAddress = "10.60.0.20";
+    hostAddress  = "10.60.1.20";
+    config = { config, pkgs, ... }:
+    {
+      networking.firewall.allowedTCPPorts = [ 2379 2380 ];
+      services.etcd = {
+        enable = true;
+        name = "wg-etcd02";
+        initialAdvertisePeerUrls = ["http://10.60.0.20:2380"];
+        listenPeerUrls           = ["http://10.60.0.20:2380"];
+        advertiseClientUrls      = ["http://10.60.0.20:2379"];
+        listenClientUrls         = ["http://10.60.0.20:2379"];
+        initialCluster           = ["wg-etcd01=http://10.60.0.10:2380"
+                                    "wg-etcd02=http://10.60.0.20:2380"];
+      };
+      systemd.services.etcd.serviceConfig.Restart = "always";
+      systemd.services.etcd.serviceConfig.TimeoutStartSec = 60;
+      systemd.services.etcd.serviceConfig.TimeoutStopSec = 60;
+    };
+  };
+
+  containers.wg-node01 = {
+    privateNetwork = true;
+    localAddress = "10.60.0.1";
+    #hostAddress  = "10.60.1.1";
+    config = { config, pkgs, ... }:
+    {
+    };
+  };
+
+  containers.wg-node02 = {
+    privateNetwork = true;
+    localAddress = "10.60.0.2";
+    #hostAddress  = "10.60.1.2";
+    config = { config, pkgs, ... }:
+    {
+    };
+  };
 }
