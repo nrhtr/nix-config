f154b48db979f31067126c85ff20dbde90204231
Jeremy Parker <jeremy@jenga.xyz>
2019-08-18 20:02:33 +1000

    minecraft: add overviewer service

--- minecraft.jenga.xyz/configuration.nix
+++ minecraft.jenga.xyz/configuration.nix
@@ -30,6 +30,15 @@
     };
   };
 
+  systemd.services.overviewer = rec {
+    description = "Update minecraft world map on minecraft.jenga.xyz"
+    startAt = "hourly"
+
+    serviceConfig = {
+      ExecStart = "${minecraft-overviewer}/bin/overviewer.py /var/lib/minecraft/world /var/www/minecraft-overviewer/";
+    };
+  }
+
   services.minecraft-server = {
     enable = true;
     declarative = true;
