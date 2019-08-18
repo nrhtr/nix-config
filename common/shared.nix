61b4068c36ba4f03922cab0a68dcc3f1e4813b88
Jeremy Parker <jeremy@jenga.xyz>
2019-08-18 19:51:32 +1000

    common: add iotop lsof

--- common/shared.nix
+++ common/shared.nix
@@ -4,7 +4,7 @@
   nixpkgs.config.allowUnfree = true;
 
   environment.systemPackages = with pkgs; [
-    vim git htop mtr mosh pass tmux gnupg magic-wormhole
+    vim git htop mtr mosh pass tmux gnupg magic-wormhole iotop lsof
   ];
 
   programs.fish.enable = true;
