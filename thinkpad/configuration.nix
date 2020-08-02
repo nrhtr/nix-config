5e92cba4bbd50d7f1d7cdeed7e34e59ada77d4fa
Jeremy Parker <jeremy@jenga.xyz>
2020-08-02 21:25:04 +1000

    thinkpad: add gateway config for tower

--- thinkpad/configuration.nix
+++ thinkpad/configuration.nix
@@ -17,11 +17,65 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "thinkpad"; # Define your hostname.
-  networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  networking.extraHosts = ''
-    10.100.0.1 nix01
-  '';
+  networking = {
+    hostName = "thinkpad"; # Define your hostname.
+    wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+    extraHosts = ''
+      10.100.0.1 nix01
+    '';
+
+    # Act as gateway for T7500
+    nat = {
+      enable = true;
+      internalIPs = [ "172.16.10.0/24" ];
+      internalInterfaces = [ "enp0s25" ];
+      externalInterface = "wlp3s0";
+    };
+
+    interfaces.enp0s25.ipv4.addresses = [ {
+      address = "172.16.10.254";
+      prefixLength = 24;
+    } ];
+
+    firewall = {
+      enable = true;
+      allowPing = true;
+      trustedInterfaces = [ "enp0s25" ];
+    };
+
+    #bridges.br0.interfaces = [ "wlp3s0" "enp0s25" ];
+  };
+
+  services.dhcpd4 = let
+    netMask            = "255.255.255.0";
+    gatewayIp          = "172.16.10.254";
+    ipRangeFrom        = "172.16.10.10";
+    ipRangeTo          = "172.16.10.253";
+    broadcastAddress   = "172.16.10.255";
+    commaSepDNSServers = "1.1.1.1";
+  in {
+    enable = true;
+    interfaces = [ "enp0s25" ];
+    extraConfig = ''
+      ddns-update-style none;
+      one-lease-per-client true;
+
+      subnet 172.16.10.0 netmask ${netMask} {
+        range ${ipRangeFrom} ${ipRangeTo};
+        authoritative;
+
+        # Allows clients to request up to a week (although they won't)
+        max-lease-time 604800;
+        # By default expire lease in 24 hours
+        default-lease-time 86400;
+
+        option subnet-mask         ${netMask};
+        option broadcast-address   ${broadcastAddress};
+        option routers             ${gatewayIp};
+        option domain-name-servers ${commaSepDNSServers};
+      }
+    '';
+  };
 
   # Use a swapfile, because we don't want to bother with another LUKS partition
   swapDevices = [
@@ -49,8 +103,8 @@
   '';
 
   # Disable the OpenSSH server.
-  services.sshd.enable = false;
-  networking.firewall.logRefusedConnections = true;
+  # services.openssh.enable = false;
+  #networking.firewall.logRefusedConnections = true;
 
   environment.systemPackages = with pkgs; [
       linuxPackages.acpi_call
